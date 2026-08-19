b502744da73b803fde0a7997d2aa1fc16fba5c73
Jamylle Santana <milysnap@gmail.com>
2026-08-19 15:12:10 -0300

    Projeto base configurado

---
+++ todo-api-java/todo-api-java/src/test/java/todo_api_java/TodoApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package todo_api_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
